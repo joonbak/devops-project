84c560b427ee75bb56d92fcd911b4f29221d70df
Joon Bak <jbdev98@gmail.com>
2025-06-16 18:16:59 +1000

    Implement and Setup monitoring with Prometheus

--- go-app/main.go
+++ go-app/main.go
@@ -4,21 +4,32 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	// "github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
+
+var requestCounter = prometheus.NewCounter(prometheus.CounterOpts{
+	Name: "myapp_http_requests_total",
+	Help: "The total number of HTTP requests",
+})
 
 type Response struct {
 	Message string `json:"message"`
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
+	requestCounter.Inc()
 	w.Header().Set("Content-Type", "application/json")
 	response := Response{Message: "Hello from Go API Server!"}
 	json.NewEncoder(w).Encode(response)
 }
 
 func main() {
+	prometheus.MustRegister(requestCounter)
 	mux := http.NewServeMux()
-
+	mux.Handle("/metrics", promhttp.Handler())
 	mux.HandleFunc("/", handler)
 
 	log.Println("Server Listening on Port 8080")
